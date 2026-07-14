4fec248e2dacd6e652f477ea0e1e8e44dcc9b0d2
yogeshsinghWAI <yogesh.singh@witting.ai>
2026-07-14 16:51:13 +0530

    LG: implemented graph stream

--- LangGraph Learning/travel_app/app/main.py
+++ LangGraph Learning/travel_app/app/main.py
@@ -15,14 +15,24 @@ def main() -> None:
         logger.info("=" * 60)
         logger.info(f"Input: {query}")
 
-        result = graph.invoke(
-            {
-                "user_input": query,
-            }
-        )
+        # result = graph.invoke(
+        #     {
+        #         "user_input": query,
+        #     }
+        # )
+        initial_state = {
+            "user_input": "Plan a trip to India",
+        }
+
+        logger.info("=" * 80)
+        logger.info(f"Input: {initial_state['user_input']}")
+
+        for event in graph.stream(initial_state):
+            logger.info(f"{event}")
+
 
         logger.success("Graph execution completed")
-        logger.info(f"Final State:\n{result['response']}")
+        # logger.info(f"Final State:\n{result['response']}")
 
 
 if __name__ == "__main__":
